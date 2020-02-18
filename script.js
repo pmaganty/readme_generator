62cde027301194ff5bbaed04445c39e3eb79b6c8
pmaganty <pranithamaganty@gmail.com>
2020-02-17 21:59:52 -0600

    edits to structure, add license

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -22,62 +23,93 @@ function promptUser() {
       message: "Please write a short description of your project."
     },
     {
-      type: "input",
+      type: "list",
       name: "license_type",
-      message: "What kind of license should your project have?" //User can choose from list of items
+      message: "What kind of license should your project have?", //User can choose from list of items
+      choices: ['Apache 2.0','BSD 3-clause', 'BSD 2-clause']
     },
     {
       type: "input",
       name: "dependency_command",
-      message: "What command should be run to install dependencies?" //(default to "npm i" if user doesn't respond)
+      message: "What command should be run to install dependencies?", //(default to "npm i" if user doesn't respond)
+      default: "npm i"
     },
     {
       type: "input",
       name: "run_test_command",
-      message: "What command should be run to run tests?" //(default to "npm test" if user doesn't respond)
+      message: "What command should be run to run tests?", //(default to "npm test" if user doesn't respond)
+      default: "npm test"
     },
     {
-    type: "input",
-    name: "usage_info",
-    message: "What does the user need to know about using the repo?" 
+      type: "input",
+      name: "usage_info",
+      message: "What does the user need to know about using the repo?" 
     },
     {
-    type: "input",
-    name: "contribution_info",
-    message: "What does the user need to know about contributing to the repo?" 
+      type: "input",
+      name: "contribution_info",
+      message: "What does the user need to know about contributing to the repo?" 
     }
   ]);
 }
 
+
 function generate_readme(answers) {
-    var header = generateHeader(answers);
     return `
-    ${header}
-    ${answers.license_type}
-    ${answers.dependency_command}
-    ${answers.run_test_command}
-    ${answers.usage_info}
-    ${answers.contribution_info}
+## Project Title: ${answers.proj_name}
+## Author (github user): ${answers.user_name}
+### Description: ${answers.proj_description}
+### Table of Contents:
+    Installation.........................<pg#>
+    Usage................................<pg#>
+    License..............................<pg#>
+    Contributing.........................<pg#>
+    Tests................................<pg#>
+
+### Installation:
+Please use the following command to install dependencies --> **${answers.dependency_command}**
+### Usage:
+Refer to the information below before using this repo:
+${answers.usage_info}
+### Licenses:
+You need the following licenses for this project:
+${answers.license_type}
+### Contributing:
+Refere to the information below before contributing to this repo:
+${answers.contribution_info}
+### Tests: 
+Please use the following command to run tests --> **${answers.run_test_command}**
+### Questions:
     `;
 }
 
-function generateHeader(answers) {
-    return `
-## Project: ${answers.proj_name}
-## Author (github user): ${answers.user_name}
-### Description: ${answers.proj_description}
-    `
-}
-  
+
 promptUser()
     .then(function(answers) {
         const readme_text = generate_readme(answers);
 
-        return writeFileAsync("README.md", readme_text);
+        axios.get('https://api.github.com/users/' + answers.user_name)
+        .then(function (response) {
+            console.log("SUCCESS GRABBED RESPONSE");
+            console.log(response.data.avatar_url);
+
+            var img_url = response.data.avatar_url;
+            var user_img = `![image info](${img_url})`;
+            var license_badge;
+            if(answers.license_type == 'Apache 2.0') {
+                license_badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+            } else if (answers.license_type == 'BSD 3-clause') {
+                license_badge= `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+            } else if (answers.license_type == 'BSD 2-clause') {
+                license_badge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+            }
+    
+            return writeFileAsync("README.md", license_badge + readme_text + user_img);
+        })
+        .then(function() {
+            console.log("Successfully wrote to README.md");
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
     })
-    .then(function() {
-        console.log("Successfully wrote to README.md");
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
